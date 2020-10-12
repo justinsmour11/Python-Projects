4a3c405493f41056e1b3f0e45ae87c0e27e4bc8d
justinsmour11 <68407200+justinsmour11@users.noreply.github.com>
2020-10-12 13:52:59 -0700

    Python classes

---
+++ classes.py
@@ -0,0 +1,12 @@
+
+class Guitar:
+    soundOrigin = 'strings'
+    pitchManipulation = 'fretboard'
+
+class ElectricGuitar(Guitar):
+    soundCapture = 'pickups'
+    amplificationAdjustments = 'knobs'
+
+class AcousticGuitar(Guitar):
+    soundAmplification = 'sound hole'
+    accessories = 'capo'

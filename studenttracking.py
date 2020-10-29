509e3d92eba92a008fe41c0b93c26b645161ca0d
justinsmour11 <68407200+justinsmour11@users.noreply.github.com>
2020-10-28 19:18:14 -0700

    Database for student contact info

---
+++ studenttracking.py
@@ -0,0 +1,58 @@
+
+
+
+from tkinter import *
+import tkinter as tk
+from tkinter import messagebox
+
+
+# Be sure to import our other modules 
+# so we can have access to them
+import studenttracking_gui
+import studenttracking_func
+
+
+# Frame is the Tkinter frame class that our own class will inherit from
+class ParentWindow(Frame):
+    def __init__(self, master, *args, **kwargs):
+        Frame.__init__(self, master, *args, **kwargs)
+
+        # define our master frame configuration
+        self.master = master
+        self.master.minsize(500,400) #(Width, Height)
+        self.master.maxsize(500,400)
+        # This CenterWindow method will center our app on the user's screen
+        studenttracking_func.center_window(self,500,400)
+        self.master.title("Student Tracking")
+        self.master.configure(bg="#F0F0F0")
+        # This protocol method is a tkinter built-in method to catch if 
+        # the user clicks the upper corner, "X" on Windows OS.
+        self.master.protocol("WM_DELETE_WINDOW", lambda: studenttracking_func.ask_quit(self))
+        arg = self.master
+
+        # load in the GUI widgets from a separate module, 
+        # keeping your code comparmentalized and clutter free
+        studenttracking_gui.load_gui(self)
+        
+        # Instantiate the Tkinter menu dropdown object
+        # This is the menu that will appear at the top of our window
+        menubar = Menu(self.master)
+        filemenu = Menu(menubar, tearoff=0)
+        filemenu.add_separator()
+        filemenu.add_command(label="Exit", underline=1,accelerator="Ctrl+Q",command=lambda: studenttracking_func.ask_quit(self))
+        menubar.add_cascade(label="File", underline=0, menu=filemenu)
+        helpmenu = Menu(menubar, tearoff=0) # defines the particular drop down colum and tearoff=0 means do not separate from menubar
+        helpmenu.add_separator()
+        helpmenu.add_command(label="How to use this program")
+        helpmenu.add_separator()
+        helpmenu.add_command(label="About This Phonebook Demo") # add_command is a child menubar item of the add_cascde parent item
+        menubar.add_cascade(label="Help", menu=helpmenu) # add_cascade is a parent menubar item (visible heading)
+        
+        self.master.config(menu=menubar, borderwidth='1')
+
+        
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    App = ParentWindow(root)
+    root.mainloop()
